bfd9646c155469b45480bbd30a56386854b6b3a5
Nahuel Maquieyra <107762777+nahue-prg@users.noreply.github.com>
2022-11-13 19:26:44 -0300

    Correccion y baja de archivo
    Se corrige query 003
    Se elimina usuarioServlet (El que usamos es ServletUsuario)

--- Querys/003-agregar-usuarios.sql
+++ Querys/003-agregar-usuarios.sql
@@ -1,14 +1,14 @@
 insert into usuarios(usuario, dni, tipoUsuario, contraseña, estado) 
 	select 	nombre as usuario, 
 			dni, 
-            1 as tipoUsuario,
+            2 as tipoUsuario,
             'clave' as clave, 
             true as estadp
 	from clientes
 	union all
     select concat(nombre,'Adm') as usuario, 
 			dni, 
-            2 as tipoUsuario, 
+            1 as tipoUsuario, 
             'clave' as clave, 
             true as estado
     from clientes
