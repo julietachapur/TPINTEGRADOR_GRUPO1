5747cf3d5cb74e60758f27a50706fe618d0849af
Lucas Fernandez <lgfernandez798@gmail.com>
2022-11-15 15:08:41 -0300

    Update 003-a-prestamos_x_autorizar.sql

--- Querys/003-a-prestamos_x_autorizar.sql
+++ Querys/003-a-prestamos_x_autorizar.sql
@@ -5,7 +5,7 @@ nroCuenta bigint(20) NOT NULL,
 fecha_creacion date NOT NULL,
 importe_pedido DECIMAL(20,6) NOT NULL,
 cantidad_cuotas INT NOT NULL ,
-estado INT NOT NULL,
+estado tinyint NOT NULL,
 primary key (codPrestamoPendinte),
 foreign key (nroCuenta) references cuentas(nroCuenta) 
 );
